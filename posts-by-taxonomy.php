9f278b1cc2672041d4de69e65b4da902939e1b66
Ben <ben@brightbridgeweb.com>
2026-05-21 15:51:16 -0600

    Fix WordPress.org deployment workflow

--- posts-by-taxonomy.php
+++ posts-by-taxonomy.php
@@ -5,7 +5,7 @@
  * @package       POSTSBYTAX
  * @author        Bright Bridge Web
  * @license       gplv2
- * @version       1.0.4
+ * @version       1.0.3
  *
  * @wordpress-plugin
  * Plugin Name:   Posts by Taxonomy
